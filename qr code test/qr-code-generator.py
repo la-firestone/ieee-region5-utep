6fd87d74f85089e276f8a044df818036ec1aceb1
amedina64 <amedina64@miners.utep.edu>
2022-12-28 21:19:22 -0700

    Added Drone QR code 12/28/22

---
+++ qr code test/qr-code-generator.py
@@ -0,0 +1,9 @@
+import qrcode
+
+
+img = qrcode.make('')
+
+type(img)
+
+img.save('drone1.png')
+
---
+++ streaming.py
@@ -0,0 +1,31 @@
+import cv2
+from djitellopy import Tello
+
+# Setup and connect Tello
+
+tello = Tello()
+tello.connect()
+tello.streamon()
+
+# Open a capture object with open CV
+cap = cv2.VideoCapture(tello.get_udp_video_address())
+
+div = 4
+
+
+try:
+    while True:
+        ret, frame = cap.read()
+        if (ret):
+            height, width, channel = frame.shape
+            frame = cv2.resize(frame, (int(width / div), int(height / div)))
+            cv2.imshow('Tello', frame)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+# When everything done, release the capture
+finally:
+    cap.release()
+    tello.streamoff()
+    cv2.destroyAllWindows()
